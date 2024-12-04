ca0aaa1e5a4efa26c272c79ee227e6d0e9154863
Fran Marín Pérez <francisco.marinperez.practicas@telefonica.com>
2024-12-04 15:30:31 +0100

    local environment fully working

--- local/web/src/web_test.py
+++ local/web/src/web_test.py
@@ -1,10 +1,15 @@
 import pytest, os
-from app import create_app
+
+from app import create_app, db
+from app.models import Data
 
 @pytest.fixture
-def client():
+def app():
 	env_name = os.getenv("FLASK_ENV", "development")
-	app = create_app(env_name)
+	return create_app(env_name)
+
+@pytest.fixture
+def client(app):
 	return app.test_client()
 
 def test_get(client):
@@ -31,3 +36,13 @@ def test_final_output(client):
 	response = client.get('/data')
 	assert response.status_code == 200
 	assert response.json == [{"id":1,"name":"SQL Test User"},{"id":2,"name":"test"}]
+
+def test_reset_data(app, client):
+	with app.app_context():
+		db.drop_all()
+		db.create_all()
+		sample_data = Data(name="SQL Test User")
+		db.session.add(sample_data)
+		db.session.commit()
+	response = client.get('/data')
+	assert response.json == [{"id":1,"name":"SQL Test User"}]
