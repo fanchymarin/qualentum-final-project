f43d5aca15d313676e0da5d8eb8499f0f068fad7
Fran Marín Pérez <francisco.marinperez.practicas@telefonica.com>
2024-12-05 17:26:25 +0100

    local fully working

--- local/web/src/web_test.py
+++ local/web/src/web_test.py
@@ -1,21 +1,20 @@
-import pytest, os
-
+import pytest
 from app import create_app, db
-from app.models import Data
 
 @pytest.fixture
 def app():
-	env_name = os.getenv("FLASK_ENV", "development")
-	return create_app(env_name)
+	return create_app("testing")
 
 @pytest.fixture
 def client(app):
+	with app.app_context():
+		db.create_all()
 	return app.test_client()
 
 def test_get(client):
 	response = client.get('/data')
 	assert response.status_code == 200
-	assert response.json == [{"id":1,"name":"SQL Test User"}]
+	assert response.json == []
 
 def test_post(client):
 	response = client.post('/data', json={'name': 'test'})
@@ -27,22 +26,12 @@ def test_post(client):
 	assert response.status_code == 409
 
 def test_delete(client):
-	response = client.delete('/data/3')
+	response = client.delete('/data/2')
 	assert response.status_code == 200
-	response = client.delete('/data/3')
+	response = client.delete('/data/2')
 	assert response.status_code == 404
 
 def test_final_output(client):
 	response = client.get('/data')
 	assert response.status_code == 200
-	assert response.json == [{"id":1,"name":"SQL Test User"},{"id":2,"name":"test"}]
-
-def test_reset_data(app, client):
-	with app.app_context():
-		db.drop_all()
-		db.create_all()
-		sample_data = Data(name="SQL Test User")
-		db.session.add(sample_data)
-		db.session.commit()
-	response = client.get('/data')
-	assert response.json == [{"id":1,"name":"SQL Test User"}]
+	assert response.json == [{'id': 1, 'name': 'test'}]
