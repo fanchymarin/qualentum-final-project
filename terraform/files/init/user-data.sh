f2c3c3b5319ac857cbfb95133c4d33ef475463c8
Fran Marín Pérez <francisco.marinperez.practicas@telefonica.com>
2024-12-08 04:52:18 +0100

    ec2 not possible, eck needed

--- terraform/files/init/user-data.sh
+++ terraform/files/init/user-data.sh
@@ -1,35 +1,18 @@
 #!/bin/bash
-#This can be used as User Data, during EC2 launch wizard
-apt update
-apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
-apt update
-apt-cache policy docker-ce
-apt install docker-ce -y
-
-#Next 2 lines are different from official Kubernetes guide, but the way Kubernetes describe step does not work
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg |  apt-key add
-echo "deb https://packages.cloud.google.com/apt kubernetes-xenial main" > /etc/apt/sources.list.d/kurbenetes.list
-
-#Turn off swap
-swapoff -a
-
-apt update
-apt install kubelet kubeadm kubectl -y
-#we need to get EC2 internal IP address- default ENI is eth0
-export ipaddr=`ip address|grep eth0|grep inet|awk -F ' ' '{print $2}' |awk -F '\/' '{print $1}'`
-
-#You can replace 172.16.0.0/16 with your desired pod network
-kubeadm init --apiserver-advertise-address=$ipaddr --pod-network-cidr=172.16.0.0/16
-
-#this adds .kube/config for root account, run same for ubuntu user, if you need it
-mkdir -p /root/.kube
-cp -i /etc/kubernetes/admin.conf /root/.kube/config
-
+snap install aws-cli --classic
 aws s3 cp s3://qualentum-dev-s3/kubernetes.yaml /root/kubernetes.yaml
-kubectl apply -f /root/kubernetes.yaml
+
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl gpg
+mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+systemctl enable --now kubelet
 
 #Uncomment bellow lines if you need to install help
 #curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
